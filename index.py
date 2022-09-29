86a5892b52e5c4d3264cd63549324bd22213e0da
Parker <pjg4086@uncw.edu>
2022-09-29 15:05:52 -0400

    added layout template and bootstrap

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return f"Welcome to Kollekt!<br>Created by Garrett McGhee, Parker Gagliano, Trent Salas, William Pridgen, and Josh Mertz."
-
+def home():
+    return render_template('home.html')
 if __name__ == '__main__':
     app.run(debug=1)
