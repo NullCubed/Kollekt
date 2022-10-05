fa91cf34c9591079647550645c2ff116dc1de284
Will Pridgen <wjp3137@uncw.edu>
2022-10-05 18:57:20 -0400

    Added collection component

--- web_app/Classes.py
+++ web_app/Classes.py
@@ -1,2 +1,12 @@
 class User:
-    def __init__(user_id, name, isAdmin):
+    def __init__(self, user_id, name, isAdmin):
+class Discussion:
+    def __init__(self):
+class Posts:
+    def __init__(self):
+class CollectionItem:
+    def __init__(self):
+class Community:
+    def __init__(self):
+class Collection:
+    def __init__(self):
