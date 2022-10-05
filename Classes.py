ee093112822fe2545f2d9a2faa153a3eb44cd0ab
trentsalas <tesalas@datacor.com>
2022-10-05 17:51:50 -0400

    made Classes.py

--- Classes.py
+++ Classes.py
@@ -1,1 +1,8 @@
-test = 1
+class userManager:
+    def getUserPermissions(user):
+        '''
+        This method returns the type of user the user is. (Admin or User)
+        :param user: The user in which you want to get User's type
+        :return: a string with users type. (Either Admin or User)
+        '''
+        pass
