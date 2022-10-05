86b0432147bd25b44506c2c172189fdf610cf1c2
trentsalas <tesalas@datacor.com>
2022-10-05 18:10:59 -0400

    Update userManager.py

--- Components/userManager.py
+++ Components/userManager.py
@@ -1,1 +1,8 @@
-test = 1
+class userManager:
+    def isAdmin(user):
+        '''
+        This method returns boolean if user is Admin
+        :param user: The user in which you want to get User's type
+        :return: boolean( True if user is Admin, False if not )
+        '''
+        pass
