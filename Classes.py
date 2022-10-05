9450ff8c6c3827d16ac6cce5636b94e1bf6cfc58
Parker <pjg4086@uncw.edu>
2022-10-05 19:00:09 -0400

    added more user methods

---
+++ Classes.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, user_id, name, isAdmin):
+        self.user_id = user_id
+        self.name = name
+        self.isAdmin = isAdmin
+
+    def getProfileInfo(self):
+        return info
+
+    def getId(self):
+        return self.user_id
+
+    def getPosts(self):
+        return posts
