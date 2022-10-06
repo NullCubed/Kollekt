93ca1cdfbe552f071683596c632d286b35238e4a
Parker <pjg4086@uncw.edu>
2022-10-06 11:48:16 -0400

    register form and register skeleton made, fixed home link

--- web_app/index.py
+++ web_app/index.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import render_template
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 
 @app.route("/")
@@ -17,6 +23,12 @@ def userProfile():
 @app.route("/community")
 def communityPage():
     return render_template('community.html')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
