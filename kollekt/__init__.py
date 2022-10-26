2e887470a7e90c9f0c1afd9430ab65eadd2ff58d
Garrett McGhee <NullCubed@users.noreply.github.com>
2022-10-26 16:32:57 -0400

    Update .gitignore Updated routes.py to correctly use local DB (sqlite)

--- kollekt/__init__.py
+++ kollekt/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cdesntysuitrba' \
