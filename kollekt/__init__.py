164351d8e2f7df807fc3444da2fc4f416c909903
Garrett McGhee <NullCubed@users.noreply.github.com>
2022-10-27 15:35:09 -0400

    Created development file (dev_script.py)

--- kollekt/__init__.py
+++ kollekt/__init__.py
@@ -1,7 +1,5 @@
-from time import sleep
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 db = SQLAlchemy()
 
@@ -13,20 +11,12 @@ def create_app():
 
     app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cdesntysuitrba' \
-    #                                         ':42ed85406d44789ac58a6b6b178f6804cb4081db461a5659cfd12c20ec172b22@ec2-52' \
-    #                                         '-200-5-135.compute-1.amazonaws.com:5432/d2inpdhdbc01g6'
-
-
+    # ':42ed85406d44789ac58a6b6b178f6804cb4081db461a5659cfd12c20ec172b22@ec2-52' \
+    # '-200-5-135.compute-1.amazonaws.com:5432/d2inpdhdbc01g6'
 
     db.init_app(app)
 
     with app.app_context():
         from . import routes
-        if os.path.isfile('instance\site.db'):
-            os.remove('instance\site.db')
-            print("Deleted local db")
-            sleep(10)
         db.create_all()
-        print("Initialized tables in local db")
-        print("** Filled tables in db with dummy info")
         return app
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from kollekt import create_app
+from dev_script import fill_db, delete_db
 
+delete_db()
 app = create_app()
 
 
