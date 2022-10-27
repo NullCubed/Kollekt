e9c53b0d3e0a7532d7f91fc8b7b93c6e9ff619dc
parker gagliano <pjg4086@uncw.edu>
2022-10-27 17:17:19 -0400

    user login

--- kollekt/__init__.py
+++ kollekt/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
-
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -16,6 +17,7 @@ def create_app():
 
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     with app.app_context():
         from . import routes
--- kollekt/routes.py
+++ kollekt/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, url_for, flash, redirect, request
 from kollekt.forms import RegistrationForm, LoginForm
 from .models import User, db
 import hashlib
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route("/")
@@ -17,8 +18,8 @@ def userProfile():
 
 @app.route("/logout")
 def logout():
-    return render_template('logout.html')
-
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route("/userSettings")
 def userSettings():
@@ -32,12 +33,18 @@ def communityPage():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash(current_user)
+        return redirect(url_for('home'))
+        
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user.password == form.password.data:
-            flash(f'Logged into {user.id}!', 'success')
-            return redirect(url_for('home'))
+        if user and user.password == form.password.data:
+            flash(f'Logged into {current_user}!', 'success')
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("Wrong Password", "Danger")
 
@@ -56,3 +63,4 @@ def register():
         flash(test.username+" "+test.email+" "+test.password, 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
