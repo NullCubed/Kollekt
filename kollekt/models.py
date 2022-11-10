2a6298a171fd863684a52426e203bf3e7cffbc68
Parker <pjg4086@uncw.edu>
2022-11-10 09:48:10 -0500

    many to many implementation

--- kollekt/models.py
+++ kollekt/models.py
@@ -9,6 +9,13 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+users_in_community = db.Table('users_in_community',
+                              db.Column('community_id', db.Integer,
+                                        db.ForeignKey('communities.id')),
+                              db.Column('user_id', db.Integer,
+                                        db.ForeignKey('user.id'))
+                              )
+
 # TODO: Move all class files into this file and setup models to initialize DB tables etc.
 #   Currently having issues with import loop ie importing db from index and then importing User from models
 #   Restructuring should resolve this issue
@@ -72,8 +79,8 @@ class Communities(db.Model):
     collections = db.relationship(
         'Collections', backref='communities', lazy=True)
 
-    def getUsers(self):
-        return ['user1', 'user2', 'user3']
+    users_in_communities = db.relationship(
+        'User', secondary=users_in_community, backref='users')
 
 
 class Photos(db.Model):
