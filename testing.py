d607c2a11ecba5ac564979d0becc4baaf32f544c
Parker <pjg4086@uncw.edu>
2022-11-29 13:58:12 -0500

    new tests

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture(scope='module')
 def app(request):
     from kollekt import create_app
@@ -28,9 +29,36 @@ def test_login(client):
     assert b'<h5 class="text-center">Dont have an account? Register now</h5>' in response.data
 
 
-
-
 def test_logged_out_homepage(client):
-
     response = client.get("/")
-    assert b"Kollekt" in response.data
+    assert b"""<h3 class="text-center" style="font-weight: bold">
+          Log in to have a personalized experience
+        </h3>""" in response.data
+
+
+def test_register(client):
+    with client:
+        response = client.post("/register", data=dict(
+            username="admin",
+            email='joe@joe.com',
+            password="admin",
+            confirm_password="admin"), follow_redirects=True)
+        assert response.status_code == 200
+        response = client.post("/login", data=dict(
+            username="admin",
+            password="admin",), follow_redirects=True)
+        assert response.status_code == 200
+
+
+def test_logged_in_homepage(client):
+    with client:
+        response = client.post("/register", data=dict(
+            username="admin",
+            email='joe@joe.com',
+            password="admin",
+            confirm_password="admin"), follow_redirects=True)
+        print(response.data)
+        # Check that there was one redirect response.
+        assert len(response.history) == 1
+        # Check that the second request was to the index page.
+        assert response.request.path == "/"
